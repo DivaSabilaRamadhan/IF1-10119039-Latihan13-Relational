39c55545fdaecdbffca0f3048557dcd4d531c986
diky <diky@diky-PC>
2020-10-17 14:11:39 +0700

    Upload Project

---
+++ src/Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * NAMA  : Diva Sabila Ramadhan 
+ * KELAS : IF1
+ * NIM   : 10119039
+ * Deskripsi Program : Program Relational
+ */
+public class Relational {
+
+   
+    public static void main(String[] args) {
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b " + (bill == bil2));
+        System.out.println("a != b " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("b <= a = " + (bil2 <= bill));
+    }
+    
+}
